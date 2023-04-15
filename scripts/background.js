bbb3eaae4a9469b0a177522aeb33e66dc9a15f6e
Ben Colbeck <benjamin.colbeck@gmail.com>
2023-04-15 15:00:02 -0700

    ben stuff

--- scripts/background.js
+++ scripts/background.js
@@ -1,20 +1,24 @@
-/// <reference types="chrome"/>
+// Background handles data collection (grabbing URL to change slides)
+let currentURL;
 
+// Runs on tab changes.
+chrome.tabs.onActivated.addListener(async function () {
+    // if (currentURL.slice(0,36) == "https://docs.google.com/presentation/") 
+    console.log("TAB CHANGED");
+    currentURL = await getTab();
+    console.log("Current URL: " + currentURL.slice(0,36));
+})
 
-// chrome.tabs.onActivated.addListener(function (tabid, windowid) {
-//     chrome.tabs.update({url: "https://www.loser.com"});
-// });
-chrome.tabs.onActivated.addListener(moveToFirstPosition);
+// Runs on tab updates.
+chrome.tabs.onUpdated.addListener(async function () {
+    console.log("TAB UPDATED");
+    currentURL = await getTab();
+    console.log("Current URL: " + currentURL);
+})
 
-async function moveToFirstPosition(activeInfo) {
-  try {
-    setTimeout(() => chrome.tabs.update({url: "https://www.coolmathgames.com"}), 50);
-    console.log("Success.");
-  } catch (error) {
-    if (error == "Error: Tabs cannot be edited right now (user may be dragging a tab).") {
-      setTimeout(() => moveToFirstPosition(activeInfo), 50);
-    } else {
-      console.error(error);
-    }
-  }
+async function getTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let tabs = await chrome.tabs.query(queryOptions);
+  return tabs[0].url;
 }
+
